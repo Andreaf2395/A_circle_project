2f83068e3d792851cdac5a9f3eab29dc37e38790
Andreaf2395 <andreaf2395>
2021-04-20 23:05:29 +0530

    added comments

--- circle.c
+++ circle.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
+//define a constant pi
 #define PI 3.14
 
 int main(){
 
 	int radius;
 	float area, circumference;
-
+	
+	//get radius as input
 	printf("Enter radius of circle: ");
 	scanf("%d",&radius);
 
+	//compute area and print
 	area= PI * radius *radius;
 	printf("Area of circle : %0.3f \n",area);
-
+	
+	//compute  circumference and print
 	circumference = 2 * PI * radius;
 	printf("Circumference of circle : %0.3f \n", circumference);
 
